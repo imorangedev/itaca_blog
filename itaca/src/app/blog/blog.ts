b5a3925c47ad3b898a61277b3ca29991fd91eb5f
orange <da.naranjo10@uniandes.edu.co>
2026-03-30 20:50:50 -0500

    blog section

--- itaca/src/app/blog/blog.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-blog',
-  templateUrl: './blog.html',
-  styleUrls: ['./blog.css'],
-  standalone: true
-})
-export class BlogComponent {}
